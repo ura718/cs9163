d05bfb6f0e0546e87129ff21c1cbf3394d865fd6
ym26 <ym26@nyu.edu>
2015-02-06 18:17:00 -0500

    added print statements to user/pass

--- passmngr.py
+++ passmngr.py
@@ -3,6 +3,7 @@
 from Crypto.Cipher import AES
 from Crypto import Random
 import argparse
+import sys
 
 
 def ECB(username):
@@ -17,39 +18,40 @@ def main():
   parser.add_argument('-ecb', action='store_true', help='use ecb encryption')
   parser.add_argument('-ctr', action='store_true', help='use ctr encryption')
   parser.add_argument('-cbc', action='store_true', help='use cbc encryption')
-  parser.add_argument('-u', help='provide username')
-  parser.add_argument('-p', help='provide password')
+  parser.add_argument('-u', dest='u', help='provide username')
+  parser.add_argument('-p', dest='p', help='provide password')
   args=parser.parse_args()  
 
-
+  #print 'username  =', args.u
+  #print 'password  =', args.p
 
   # OPTIONS 
-
   if args.ecb:
     c_ecb = args.ecb
-
-
   if args.ctr:
     c_ctr = args.ctr
-
-
   if args.cbc:
     c_cbc = args.cbc
 
-
-  if args.u:
+  if args.u == None:
+    print "[-] username required "
+    sys.exit(0)
+  else:
+    print "[+] username provided "
     username = args.u
 
-
-  if args.p:
+  if args.p == None:
+    print "[-] password required "
+    sys.exit(0)
+  else:
+    print "[+] password provided "
     password = args.p
-    
 
 
 
 
   
-
+    
 
  
 
