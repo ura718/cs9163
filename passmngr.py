e8b6c1aefb8978022fda3c1dfa59106239b293e1
ym26 <ym26@nyu.edu>
2015-02-06 17:31:29 -0500

    updated options feature

--- passmngr.py
+++ passmngr.py
@@ -24,25 +24,30 @@ def main():
 
 
   # OPTIONS 
+
   if args.ecb:
     c_ecb = args.ecb
+
 
   if args.ctr:
     c_ctr = args.ctr
 
+
   if args.cbc:
     c_cbc = args.cbc
 
+
   if args.u:
     username = args.u
+
 
   if args.p:
     password = args.p
     
 
 
-  #if c_ecb and username and password:
-  #  print "got it"
+
+
   
 
 
