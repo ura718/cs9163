3e1307de6abbe4398c2503c0eff4dd2e9330c6ff
ym26 <ym26@nyu.edu>
2015-02-08 01:04:59 -0500

    added static cryptography and randomized cryptography

---
+++ encryption_AES.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+from Crypto.Cipher import AES
+from Crypto import Random
+
+
+
+
+''' Here we randomize initialization vector (iv) and append it to key '''
+
+def RANDOM_ENCRYPTION():
+  key = b'Sixteen byte key'
+  iv = Random.new().read(AES.block_size)
+
+  # Encrypt
+  encryption_suite = AES.new(key, AES.MODE_CFB, iv)
+  cipher_text = iv + encryption_suite.encrypt(b'Attack at dawn')
+  print cipher_text
+
+
+  # Decrypt
+  encryption_suite = AES.new(key, AES.MODE_CFB, iv)
+  plain_text = iv + encryption_suite.decrypt(cipher_text)
+  print plain_text
+
+
+
+
+
+
+''' Here the initialization vector (iv) is static and append it to key '''
+
+def STATIC_ENCRYPTION():
+
+  # key = b'Sixteen byte key'
+  # iv = 'This is an IV456'
+
+
+  ''' Encryption - cipher_text must be of 16 byte multiple (e.g: 16, 32, 48...etc) '''
+  encryption_suite = AES.new('Sixteen byte key', AES.MODE_CBC, 'This is an IV456')
+  cipher_text = encryption_suite.encrypt("A really secret message... Not for prying eyes!!")
+  print cipher_text
+
+  # Decryption
+  decryption_suite = AES.new('Sixteen byte key', AES.MODE_CBC, 'This is an IV456')
+  plain_text = decryption_suite.decrypt(cipher_text)
+  print plain_text
+
+
+
+
+
+RANDOM_ENCRYPTION()
+#STATIC_ENCRYPTION()
+
+
