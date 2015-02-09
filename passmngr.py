8f08311f99da6db90dadbbf8f441f2d0f1e2ec79
ym26 <ym26@nyu.edu>
2015-02-09 10:07:55 -0500

    no comment

--- passmngr.py
+++ passmngr.py
@@ -4,10 +4,69 @@ from Crypto.Cipher import AES
 from Crypto import Random
 import argparse
 import sys
+import os
 
 
-def ECB(username):
-  print "%s" % (username)
+#
+# input user/pass and place encrypted pass into file
+# you then take another input for user/pass and then 
+# open the password file decrypt the pass for where user 
+# is the same match up the passwords and if they are same
+# then dont put it in. Otherwise put it in
+#
+
+
+
+''' This function checks for duplicate entries in dbpass file '''
+
+def CHKDUP(username, password):
+  try:
+    if os.path.exists('./dbpass'):
+      print "check for duplicates entries"
+      
+  except OSerror, e:
+    pass
+  
+    
+
+
+
+
+
+def DB(username, password):
+
+  # Verify if credentials already exists in file before writing
+  CHKDUP(username, password) 
+
+  print "Write credentials to db file"
+  
+
+  # open file for writing even if file does not exist
+  cred = open('dbpass', 'a')
+  cred.write(("%s:%s") % (username, password))
+  cred.write("\n")
+  cred.close()
+
+
+
+
+def ECB(username, password):
+  print "ECB selected: %s:%s" % (username,password)
+  DB(username, password) 
+
+
+
+def CTR(username, password):
+  print "CTR selected: %s:%s" % (username,password)
+  DB(username, password)
+
+
+
+def CBC(username, password):
+  print "CBC selected: %s:%s" % (username,password)
+  DB(username, password)
+
+
 
 
 
@@ -22,9 +81,10 @@ def main():
   parser.add_argument('-p', dest='p', help='provide password')
   args=parser.parse_args()  
 
+  
 
   # OPTIONS 
-  ''' setting flag to coordinate and see if encryption was selected '''
+  ''' setting flag to check if encryption was selected '''
 
   flag = False
   if args.ecb:
@@ -38,12 +98,9 @@ def main():
     flag = True
 
 
-
   if flag == False:
     print "[-] encryption required"
     sys.exit(0)
-  elif flag == True:
-    print "[+] encryption provided"
 
 
    
@@ -52,25 +109,29 @@ def main():
     print "[-] username required "
     sys.exit(0)
   else:
-    print "[+] username provided "
     username = args.u
 
   if args.p == None:
     print "[-] password required "
     sys.exit(0)
   else:
-    print "[+] password provided "
     password = args.p
 
 
 
 
-  
+
+
+  # BEGIN ENCRYPTION based on selected attributes
+  if args.ecb:
+    ECB(username, password)
+  elif args.ctr:
+    CTR(username, password)
+  elif args.cbc:
+    CBC(username, password) 
     
 
  
-
-   
 
 
 if __name__ == "__main__":
