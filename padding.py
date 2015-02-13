b7d7e2d75359bf056eb27c17257848091baeca4d
ym26 <ym26@nyu.edu>
2015-02-12 23:18:42 -0500

    update padding counter

--- padding.py
+++ padding.py
@@ -11,18 +11,18 @@ msg=raw_input()
 
 padding=len(msg)
 
-counter=1
+counter=0
 
 while True:
   if len(msg) == 0:
     print "message is empty"
     break
   elif padding % 16 != 0:
+    counter = counter + 1
     padding = len(msg) + counter   
     print "%s + %s " % (len(msg),counter)
-    counter = counter + 1
   elif padding % 16 == 0:
     break
 
 # print message
-print 'msg: %s + padding: %s' % (len(msg), counter-1)
+print 'msg: %s + padding: %s' % (len(msg), counter)
